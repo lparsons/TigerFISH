78166ee2cfedbc152213ecbb68afa642840e7c96
nslavov <devnull@localhost>
2010-07-29 12:16:31 +0000

    Added approximation for cells with many spots

--- spotProb.m
+++ spotProb.m
@@ -1,6 +1,17 @@
 function prob = spotProb( rna )
 
+% Function for computing the probability that a cell has N number of mRNAs (returned in vector prob) given the probability for each spot to be an mRNA in vector (rna)
+%  prob = spotProb( 0.5+0.5*rand(1,10) )
+
 if nargin < 1, rna =  [ .9, .8, .7, .88, .65, .75 ]; end 
+
+% If the number of spots is large, computing the full Multinomial becomes rather expensive. 
+% To avoid combinatoril increase in complexity, the spots are grouped into N number of grous
+N = 12;
+if numel( rna ) > N
+	rna1 = rna;
+	[IDX, rna] = kmeans(rna, N)
+end
 
 no_rna = 1 - rna;
 
