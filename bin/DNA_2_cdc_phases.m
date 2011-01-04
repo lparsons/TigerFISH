867ecc98ad7e66922ba0c0d57dacb6c9911334aa
lparsons <devnull@localhost>
2011-01-04 17:29:47 +0000

    Fixed PDF output margins for jointDist functions and DNA_2_cdc_phases Changed normalization of variance in DNA_2_cdc_phases to divide by std instead of var

--- bin/DNA_2_cdc_phases.m
+++ bin/DNA_2_cdc_phases.m
@@ -1,45 +1,27 @@
 function [cdc_phases cdc_phases_p] = DNA_2_cdc_phases( DNA_Content, MaxIter, PathFileName )
 
-
-
-%Sets a Default
+% Set MaxIter default
 if nargin ==1 || isempty(MaxIter), MaxIter = 1e2; end 
 
-%%
 % Normalize variance to avoid problems from treating pdf as pmf
-Var = var(  DNA_Content( ~isnan(DNA_Content) )  );
-DNA_Content = DNA_Content * (1/Var ); 
+Std = std(  DNA_Content( ~isnan(DNA_Content) )  );
+DNA_Content = DNA_Content * (1/Std );
+var(DNA_Content);
  
 % Generate initial expectations and IC for starting an EM optimization
 Median = median( DNA_Content );
 muG1 = 0.6*Median;
-
 sigG1 = std( DNA_Content( DNA_Content< 1.4*muG1 ) );
-
-
 
 for i=1:MaxIter
     
-    %Computes probability for each cells to be in G1 or G2
-    
+    % Computes probability for each cells to be in G1 or G2
     pG1 = normpdf( DNA_Content,   muG1,   sigG1 ); 
     pG2 = normpdf( DNA_Content, 2*muG1, 2*sigG1 ); 
     
-    %Computes cumulative probability for each cells to be in G1 or G2
-    
+    % Computes cumulative probability for each cells to be in G1 or G2
     pcG1 = normcdf( DNA_Content,   muG1,   sigG1 ); 
     pcG2 = normcdf( DNA_Content, 2*muG1, 2*sigG1 );     
-    
-%     
-%      
-%     % Uses the probabilities to reassign cells into G1 and G2
-%     indG1a = find( pG1> pG2 & pG1 > 0.6 );
-%     indG1b = find( DNA_Content< muG1 & pG1 > 0.2 ); 
-%     indG1 = [indG1a; indG1b];
-%     indG2a = find( pG1< pG2 & pG2 > 0.6 );
-%     indG2b = find( DNA_Content> 2*muG1 & pG2 > 0.2 );
-%     indG2 = [indG2a; indG2b];
-    
      
     % Uses the probabilities to reassign cells into G1 and G2
     indG1b = find( DNA_Content< muG1 & pcG1 > 0.01 );    
@@ -49,15 +31,12 @@ for i=1:MaxIter
     indG2a = find( DNA_Content< 2*muG1 & pcG2 > 0.2  & pG1< pG2 );
     indG2 = [indG2a; indG2b];
     
-    
-    
     % Updates the expectations muG1 and sigG1 based on 
     muG1 = mean( DNA_Content(indG1) ); 
     muG2 = mean( DNA_Content(indG2) ); 
     muG1 = mean( muG1 + 0.5*muG2 );        % fprintf( '%1.2f\n', muG1 );
     if muG1>Median, muG1=muG1/2; end
     if muG1<0.1*Median, muG1=2*muG12; end
-    
     
     sigG1 = std( DNA_Content(indG1) ); 
     sigG2 = std( DNA_Content(indG2) ); 
@@ -68,7 +47,6 @@ for i=1:MaxIter
     end
     pG1_old = pG1;
 end 
-
 
 cdc_phases = zeros( size(DNA_Content) );  
 cdc_phases( indG1 ) = 1;
@@ -120,30 +98,14 @@ if nargin >= 3 && ~isempty(PathFileName)
     set(h, 'fontsize',       24,...
            'interpreter',   'tex'       );  
     
-    
-    set( gcf, 'Position', [440  159  692   419] )
-    set( gca, 'Position',  [0.14 0.14 0.80  0.78] );
+    %set( gcf, 'Position', [440  159  692   419] )
+    %set( gca, 'Position',  [0.14 0.14 0.80  0.78] );
     set( gca, 'FontSize', 14, 'FontWeight', 'Bold' );
-    set( gcf, 'PaperSize', [8.5 5],...
-              'PaperPositionMode', 'auto' );
+    set( gcf, 'PaperSize', [8 5],...
+              'PaperPositionMode', 'manual',...
+	      'PaperPosition', [.1 .1 7.8 4.8] );
 
     print( '-dpdf', PathFileName );
       
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- bin/jointDist.m
+++ bin/jointDist.m
@@ -65,44 +65,35 @@ else
 end
 spx = round( MAXx/10 );
 spy = round( MAXy/10 );
+
 main_fig = figure('Visible', 'off');
-sz = 0.65;
-sz2   = 0.22;   
+set(main_fig,'PaperPositionMode','auto', 'PaperSize', [8  8], 'Units', 'inches')
 
 
-h1 = axes( 'Position',    [0.1        0.1       sz         sz ]  );
-imagesc( [0 MAXx], [MAXy 0], log2(p_1_2(IN.y,IN.x)+1) ); 
-
+% Plot main image
+h1 = axes( 'Position',    [0.1       0.1      0.65     0.65 ], 'Parent', main_fig, 'Units', 'normalized'  );
+imagesc( [0 MAXx], [MAXy 0], log2(p_1_2(IN.y,IN.x)+1), 'Parent',h1 ); 
 set(h1, 'Ytick', 0:spy:MAXy, 'YtickLabel', MAXy:-spy:0 );
 set(h1, 'Xtick', 0:spx:MAXx, 'XtickLabel', 0:spx:MAXx )
+set(h1, 'FontWeight', 'Bold', 'FontSize', 12 );
+% Set Clim for imagesc plot
+p_1_2
+p_1_2_sorted = sort( log2(p_1_2(p_1_2>0)) )
+color_lims = [0 max(1, p_1_2_sorted(end-2))]
+set(h1, 'Clim', color_lims ); 
+% Set axis lables
+h(1) = xlabel( Gene1 );
+h(2) = ylabel( Gene2 );
 
-%colormap( bone );
+
+% Colormap
 BONE = bone;
 colormap( BONE(end:-1:1,:) );
- 
 h_cb = colorbar;
-set( h_cb, 'Position', [ sz+0.15   sz+0.12    0.4*sz2    sz2] )
-p_1_2_sorted = sort( log2(p_1_2(p_1_2>0)) );
-set(h1, 'Clim', [0  max(1, log2(p_1_2_sorted(end-2))) ] ); 
+set( h_cb, 'Position', [ 0.76   0.76    0.1    0.2] , 'Parent', main_fig, 'Units', 'normalized')
+caxis(color_lims)
 
-
-%Segments = regexp(File_Name, '_', 'split' );
-%Title = regexprep(File_Name, '_', ' : ' );
-% Title = sprintf( '%s : %s :: $R=%+1.2f$',...
-%           Segments{2}, ...
-%           Segments{3},  r );
-%Title = sprintf( 'Correlation: $%+1.2f$', r );
-          
-set(h1, 'FontWeight', 'Bold', 'FontSize', 12 );
-
-h(1) = xlabel( Gene1 );    
-h(2) = ylabel( Gene2 );   
-%sett( h(1:2), 20 ); 
-%set(h(1:2), 'fontsize', 18, 'FontWeight', 'bold');
-
-%h(3) = title( Title ); 
-%set( h(3), 'FontSize', 20, 'interpreter',   'latex' );
-
+% Log_2(Cells) Summary calculation
 bins = 0:MAX;
 Errors1 = sqrt(p1);
 Errors2 = sqrt(p2);
@@ -111,42 +102,37 @@ if 1
     p2 = log2(p2);   Errors2 = 0.3*p2/2;
 end
 
-h2 = axes( 'Position',    [ 0.1       sz+0.11   sz     sz2 ]  );
+% Log_2(Cells) Summary Y-Axis (Gene2)
+h2 = axes( 'Position',    [ 0.1      0.76   0.65     0.2 ] , 'Parent', main_fig, 'Units', 'normalized' );
 bar(0:MAXy, p2(IN.y) ); hold on
-%errorbar( 0:MAXx, p2(IN.x), Errors2(IN.x), 'r.' );
 ylim( [0 1.02*max( p2(IN.y) )]);
 xlim( [0-0.5  MAXy+0.5]);
 set(h2, 'Xtick', [] );
 set(h2, 'FontWeight', 'Bold' );
 h(3) = ylabel( 'log_2(Cells)' );
-%==========================================================================
 
 
-
-
-h3 = axes( 'Position',    [sz+0.11    0.1       sz2     sz ]  );
+% Log_2(Cells) Summary X-Axis (Gene1)
+h3 = axes( 'Position',    [0.76    0.1       0.2     0.65 ] , 'Parent', main_fig, 'Units', 'normalized' );
 barh(0:MAXx, p1(IN.x) ); hold on
-%errorbar_x( p1(IN.y), 0:MAXy, Errors1(IN.y), 'r.' );
 xlim( [0 1.02*max( p1(IN.x) )]);
 ylim( [0-0.5  MAXx+0.5]);
 set(h3, 'Ytick', [] );
 set(h3, 'FontWeight', 'Bold' );
-
 h(4) = xlabel( 'log_2(Cells)' ); 
+
+
+% Set fontsize of axis labels
 set(h, 'fontsize',   20, 'fontWeight', 'Bold' );
-%==========================================================================
-%sett( h, 23 ); 
 
 
-set( gcf, 'Position', [357   245   670   571] ) 
-% set( gcf, 'Position', [440  359  692   419] )
-% set( gca, 'Position', [0.14 0.14 0.80  0.78] );
-set( gcf, 'PaperSize', [7.5  6.5],...
-          'PaperPositionMode', 'auto' );
-%Hhh = gtext( r  ); sett(Hhh, 34 );    
+% Print figure if filename specified
 if nargin >=5
-    print(main_fig, '-dpdf', Folder_File_Name );
-    %system( [ 'start ' Folder_File_Name '.pdf'] );
+	set(main_fig,'InvertHardcopy','on')
+	set(gcf, 'PaperPositionMode', 'manual');
+	set(gcf, 'PaperUnits', 'inches');
+	set(gcf, 'PaperPosition', [.25 .25 7.5 7.5]);
+	print(main_fig, '-dpdf', Folder_File_Name, '-r0' );
 end 
 
 % Computting Entropy & Mutual Information
