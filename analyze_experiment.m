1736932c2c9df6435d1f16a5b158bb6feb93328e
Lance Parsons <lparsons@princeton.edu>
2012-05-16 10:51:51 -0400

    Fixed aspect ration of spot intensity histograms when run on headless server

--- analyze_experiment.m
+++ analyze_experiment.m
@@ -157,8 +157,10 @@ for d=1:size(dyes,1)
         histogram.(dye) = spot_intensity_histogram(spot_intensities(out_spots_ind), spot_intensities(in_spots_ind), threshold.(dye), ...
             'max', ip.Results.histogram_max{d});
         title([strrep(dyelabel, '_', '.') ' Spot Intensity Histogram'], 'FontSize', 22);
-        set(histogram.(dye),'PaperPositionMode','auto', 'PaperSize', [10 5], 'Units', 'inches')
-        set(histogram.(dye), 'Position',  [.25 .25 9.5 4.5] );
+        set(histogram.(dye), 'PaperPositionMode', 'manual');
+        set(histogram.(dye), 'PaperUnits', 'inches');
+        set(histogram.(dye), 'PaperSize',  [10 5] );
+        set(histogram.(dye), 'PaperPosition',  [.25 .25 9.5 4.5] );
         print(histogram.(dye), '-dpdf', [ip.Results.output_dir filesep dye '_spot_intensity_histogram.pdf'], '-r0');
         close(histogram.(dye))
         
